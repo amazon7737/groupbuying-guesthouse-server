68ae5e345a4fb30a833693ee8db52d0f84034fa3
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-07-09 17:44:53 +0900

    프로젝트 생성

---
+++ src/test/java/org/example/guesthouse/GuesthouseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.guesthouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuesthouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
