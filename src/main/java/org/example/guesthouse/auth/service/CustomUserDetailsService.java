edee2cf610f004469546f2911cbbed341c848b8a
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-07-21 18:23:06 +0900

    feat: username 으로 로그인 매칭하도록 customUserDetail 로직 수정

---
+++ src/main/java/org/example/guesthouse/auth/service/CustomUserDetailsService.java
@@ -0,0 +1,46 @@
+package org.example.guesthouse.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.guesthouse.user.infrastructure.User;
+import org.example.guesthouse.user.infrastructure.UserDataRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Component("userDetailService")
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserDataRepository userRepository;
+
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(final String username){
+        return userRepository.findOneWithAuthoritiesByUsername(username)
+                .map(user -> createUser(username, user))
+                .orElseThrow(() -> new UsernameNotFoundException(username + " -> 데이터베이스에서 찾을 수 없습니다."));
+    }
+
+    // user 객체 생성 메소드
+    private org.springframework.security.core.userdetails.User createUser(String username, User user){
+        if(!user.isActivated()){
+            throw new RuntimeException(username + " -> 활성화되어 있지 않습니다.");
+        }
+
+        List<GrantedAuthority> grantedAuthorities = user.getAuthorities().stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getAuthorityName()))
+                .collect(Collectors.toList());
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),
+                user.getPassword().getPassword(),
+                grantedAuthorities);
+    }
+}
