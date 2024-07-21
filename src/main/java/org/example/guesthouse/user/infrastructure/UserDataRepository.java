0e8331993118b64b6b2a3ac39a754553c558b144
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-07-21 18:25:45 +0900

    feat: User Data Jpa repository 코드 작성

---
+++ src/main/java/org/example/guesthouse/user/infrastructure/UserDataRepository.java
@@ -0,0 +1,16 @@
+package org.example.guesthouse.user.infrastructure;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserDataRepository extends JpaRepository<User, Long> {
+
+    @EntityGraph(attributePaths = "authorities")
+    Optional<User> findOneWithAuthoritiesByUsername(String username);
+
+
+}
