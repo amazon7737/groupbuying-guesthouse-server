c38b79e86af13f5d69156f0b8bf4f173cf146cd5
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-06 16:27:23 +0900

    feat: 예약대기중인 유저 조회 메소드 작성

--- src/main/java/org/example/guesthouse/Reservation/infrastructure/ReservationDataRepository.java
+++ src/main/java/org/example/guesthouse/Reservation/infrastructure/ReservationDataRepository.java
@@ -1,8 +1,15 @@
 package org.example.guesthouse.Reservation.infrastructure;
 
+import org.example.guesthouse.user.infrastructure.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ReservationDataRepository extends JpaRepository<Reservation, Long> {
+
+    Optional<Reservation> findReservationByWaitingUsers(User user);
+
+
 }
