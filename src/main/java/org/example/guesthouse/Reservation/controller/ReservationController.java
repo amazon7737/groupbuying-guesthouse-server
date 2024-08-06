707298a23b0279367bac060f493f33c8f1801460
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-06 16:38:46 +0900

    feat: 마이페이지 정보에 관한 dto 작성

---
+++ src/main/java/org/example/guesthouse/Reservation/controller/ReservationController.java
@@ -0,0 +1,26 @@
+package org.example.guesthouse.Reservation.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.guesthouse.Reservation.service.ReservationService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    // 함께 예약하기 -> 게스트 방 선택
+
+    // 사용자가 예약한 하우스 조회
+
+    // 사용자가 하우스 예약하기
+
+    // 사용자가 하우스 취소하기
+
+    // 지역, 날짜, 적게 남은 예약 순서로 조회 가능
+
+
+
+
+}
