a56afc3280324cdfbc02672c1615f6f1c1aa0700
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-06 16:37:29 +0900

    feat: 유저 service에 관한 테스트코드 작성

---
+++ src/test/java/org/example/guesthouse/service/UserServiceTest.java
@@ -0,0 +1,47 @@
+package org.example.guesthouse.service;
+
+import org.example.guesthouse.user.dto.request.SignupRequest;
+import org.example.guesthouse.user.dto.response.UserInfo;
+import org.example.guesthouse.user.infrastructure.User;
+import org.example.guesthouse.user.service.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    UserService userService;
+
+    @Test
+    @Transactional
+    void signUpTest(){
+        // given
+        String username = "amazon7737";
+        String password = "1234";
+        String nickname = "아마존";
+
+        // when
+        userService.join(SignupRequest.of(username, password, nickname));
+        UserInfo user = userService.get(username);
+
+        // then
+        Assertions.assertEquals(username, user.username());
+
+    }
+
+
+
+    @Test
+    void loginTest(){
+
+    }
+
+}
