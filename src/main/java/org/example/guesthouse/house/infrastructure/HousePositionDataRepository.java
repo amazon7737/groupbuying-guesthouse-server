9a243ad28efc0eef84c7e939df2b2bccceb9b3a4
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-08 19:05:27 +0900

    feat: house id값으로 해당 좌표를 조회하는 메소드 작성

--- src/main/java/org/example/guesthouse/house/infrastructure/HousePositionDataRepository.java
+++ src/main/java/org/example/guesthouse/house/infrastructure/HousePositionDataRepository.java
@@ -5,4 +5,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HousePositionDataRepository extends JpaRepository<HousePosition, Long> {
+
+    HousePosition findByHouseId(HouseInfo houseInfo);
+
 }
