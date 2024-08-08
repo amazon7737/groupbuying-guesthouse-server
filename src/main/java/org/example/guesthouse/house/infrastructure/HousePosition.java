b2ac7f820887d7a921a171afd1137b386b755eab
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-08 19:05:10 +0900

    fix: Builder 어노테이션 추가 , Double 타입으로 좌표 속성값 변경

--- src/main/java/org/example/guesthouse/house/infrastructure/HousePosition.java
+++ src/main/java/org/example/guesthouse/house/infrastructure/HousePosition.java
@@ -2,6 +2,7 @@ package org.example.guesthouse.house.infrastructure;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.guesthouse.auth.util.BaseEntity;
@@ -10,13 +11,14 @@ import org.example.guesthouse.auth.util.BaseEntity;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class HousePosition extends BaseEntity {
 
     @Column(nullable = false, unique = false)
-    private String latitude;
+    private Double latitude;
 
     @Column(nullable = false,unique = false)
-    private String longtitude;
+    private Double longitude;
 
 
     @ManyToOne(fetch = FetchType.LAZY)
