2ebfcd386576c974a8d812cac930a45b738749f2
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-10 17:40:54 +0900

    feat: 하우스 전체 조회 v2 controller 작성

---
+++ src/main/java/org/example/guesthouse/house/controller/HouseController.java
@@ -0,0 +1,53 @@
+package org.example.guesthouse.house.controller;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.example.guesthouse.house.dto.response.HouseImageResponse;
+import org.example.guesthouse.house.dto.response.HouseInfoResponse;
+import org.example.guesthouse.house.infrastructure.HouseImage;
+import org.example.guesthouse.house.infrastructure.HouseInfo;
+import org.example.guesthouse.house.service.HouseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+@RestController
+@RequiredArgsConstructor
+public class HouseController {
+
+    private final HouseService houseService;
+
+    @GetMapping("/house")
+    public ResponseEntity<List<HouseInfoResponse>> findHouseAll(){
+
+        List<HouseInfoResponse> list = houseService.findHouseInfoAndImagesAndPositionAll();
+
+        return ResponseEntity.status(HttpStatus.OK).body(list);
+
+    }
+
+    @GetMapping("/house/v2")
+    public ResponseEntity<List<HouseInfoResponse>> findHouseAllWithAsync() throws ExecutionException, InterruptedException {
+        List<HouseInfoResponse> list = houseService.findByHouseInfoV2();
+        return ResponseEntity.status(HttpStatus.OK).body(list);
+    }
+
+
+    // 하우스 등록하기
+
+    // 하우스 수정
+
+    // 가계 폐점 -> 완전 폐점 기한
+
+    // 예약 문의 조회
+
+    // 예약 문의 답변달기
+
+
+
+}
