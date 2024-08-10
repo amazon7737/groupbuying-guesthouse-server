4d4c0bd82e0ed2a445b13fa0b80edd86ad81bba5
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-10 23:42:58 +0900

    fix: 병렬 쿼리 로직 수정 , 중복되서 데이터 나오는 문제 해결

--- src/main/java/org/example/guesthouse/house/service/HouseService.java
+++ src/main/java/org/example/guesthouse/house/service/HouseService.java
@@ -5,15 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.guesthouse.house.dto.response.HouseInfoResponse;
 import org.example.guesthouse.house.infrastructure.*;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 @Service
 @RequiredArgsConstructor
@@ -34,7 +33,7 @@ public class HouseService {
         for(int i=0; i< houseInfoList.size(); i++){
 
             List<HouseImage> imageList = houseImageDataRepository.findAllByHouse(houseInfoList.get(i));
-            result.add(HouseInfoResponse.of(houseInfoList.get(i), imageList ));
+            result.add(HouseInfoResponse.of(HouseInfoResponse.from(houseInfoList.get(i)), imageList ));
         }
 
         return result;
@@ -47,56 +46,98 @@ public class HouseService {
         return result;
     }
 
+
+
+
     @Transactional(propagation = Propagation.NOT_SUPPORTED, readOnly = true)
-    @Async
-    public List<HouseImage> setDataV2(HouseInfo houseInfo){
+    @Async("threadPoolTaskExecutor")
+    public CompletableFuture<List<HouseImage>> setDataV2(HouseInfo houseInfo)  {
 
-        log.info("========== get Data {}=========== ", houseInfo.getId());
+        log.info("========== async start {}=========== ", houseInfo.getHouseName());
+//        Thread.sleep(4000);
 
+        CompletableFuture result =  CompletableFuture.completedFuture(houseImageDataRepository.findAllByHouse(houseInfo));
 
-        return houseImageDataRepository.findAllByHouse(houseInfo);
-
-    }
-
-
-    @Transactional
-    public List<HouseInfoResponse> findByHouseInfoV2() throws ExecutionException, InterruptedException {
-        List<HouseInfoResponse> result = new ArrayList<>();
-
-        List<List<HouseImage>> houseImageList = new ArrayList<>();
-
-        CompletableFuture<List<HouseImage>> future = new CompletableFuture<>();
-
-        List<HouseInfo> houseInfoList = findAll();
-
-        log.info("========== async start =========== ");
-
-        for(int i=0; i< houseInfoList.size();i++){
-
-            int finalI = i;
-
-            Executors.newCachedThreadPool().submit(() -> {
-
-                log.info("========== 쓰레드온 =========== {}", houseInfoList.get(finalI));
-
-            future.complete(setDataV2(houseInfoList.get(finalI)));
-        });
-
-
-        houseImageList.add(future.get());
-
-        }
-
-        log.info("========== async end =========== ");
-
-        for(int i=0; i< houseInfoList.size(); i++){
-            result.add(HouseInfoResponse.of(houseInfoList.get(i), houseImageList.get(i)));
-        }
+        log.info("========== async end {}=========== ", houseInfo.getHouseName());
 
         return result;
 
     }
 
 
+    @Transactional(propagation = Propagation.NOT_SUPPORTED, readOnly = true)
+    @Async("threadPoolTaskExecutor")
+    public Future<List<HouseImage>> setDataV3(HouseInfo houseInfo){
+        log.info("========== async start {}=========== ", houseInfo.getHouseName());
+        Future result = new AsyncResult(houseImageDataRepository.findAllByHouse(houseInfo));
+        log.info("========== async end {}=========== ", houseInfo.getHouseName());
 
+        return result;
+
+    }
+
+
+    @Transactional(readOnly = true)
+    public HouseInfoResponse[] findByHouseInfoV2() throws ExecutionException, InterruptedException {
+
+        List<List<HouseImage>> houseImageList = new ArrayList<>();
+
+        List<CompletableFuture> futureList = new ArrayList<>();
+
+        List<HouseInfo> houseInfoList = findAll();
+
+        HouseInfoResponse[] result = new HouseInfoResponse[houseInfoList.size()];
+
+        for(int i=0; i< houseInfoList.size();i++){
+
+        CompletableFuture<List<HouseImage>> futures = new CompletableFuture<>();
+
+            int finalI1 = i;
+            futures.thenAccept(s -> {
+
+                try {
+                    houseImageList.add(futures.get());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+
+                for(int j =0; j<houseImageList.size(); j++){
+
+                    result[finalI1] = HouseInfoResponse.of(HouseInfoResponse.from(houseInfoList.get(finalI1)), houseImageList.get(j));
+
+                }
+
+            });
+
+        log.info("========== 111 =========== ");
+
+            int finalI = i;
+            Executors.newCachedThreadPool().submit(() -> {
+                    try{
+
+                        log.info("======= method start ======");
+                        futures.complete(setDataV2(houseInfoList.get(finalI)).get());
+                        log.info("======== method end ======== ");
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    }
+
+                });
+
+        }
+
+
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(futureList.toArray(new CompletableFuture[0]));
+        allOf.join();
+
+        log.info("========== 333 =========== ");
+
+        return result;
+
+    }
 }
