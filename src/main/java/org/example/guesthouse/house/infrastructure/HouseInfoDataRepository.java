50f88abc9cb097762c5bf52578eac37275a0fcb9
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-09-21 23:32:35 +0900

    feat: Pageable 기능 추가된 메소드 작성

--- src/main/java/org/example/guesthouse/house/infrastructure/HouseInfoDataRepository.java
+++ src/main/java/org/example/guesthouse/house/infrastructure/HouseInfoDataRepository.java
@@ -1,8 +1,19 @@
 package org.example.guesthouse.house.infrastructure;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface HouseInfoDataRepository extends JpaRepository<HouseInfo, Long> {
+
+
+    @Query("select m from HouseInfo m join fetch m.user")
+    Page<HouseInfo> findAll(Pageable pageable);
+
+
 }
