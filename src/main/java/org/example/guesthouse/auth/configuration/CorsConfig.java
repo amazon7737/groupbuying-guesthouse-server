67bafdf65d5e3e8d6511e767d09c686a3360322f
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-12-28 19:17:30 +0900

    fix: 주입되는 클래스인 config -> configuration 네이밍 변경

--- src/main/java/org/example/guesthouse/auth/configuration/CorsConfig.java
+++ src/main/java/org/example/guesthouse/auth/configuration/CorsConfig.java
@@ -1,4 +1,4 @@
-package org.example.guesthouse.auth.config;
+package org.example.guesthouse.auth.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/org/example/guesthouse/auth/configuration/SwaggerConfig.java
+++ src/main/java/org/example/guesthouse/auth/configuration/SwaggerConfig.java
@@ -1,14 +1,11 @@
-package org.example.guesthouse.auth.config;
+package org.example.guesthouse.auth.configuration;
 
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * localhost:8080/swagger-ui/index.html/
